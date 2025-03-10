c6870679703de5d36c9838292751d21348b0cfc6
Prajwal H S <prajwalhs4india@gmail.com>
2025-03-10 18:58:37 +0530

    added tss

--- src/components/Footer/BottomFooter.tsx
+++ src/components/Footer/BottomFooter.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FooterSocial from "./FooterSocial";
-
+import Image from "next/image";
+import tss from "@/media/Images/TSS.png"
 const BottomFooter = () => {
   return (
     <div className="bottomFooter-container">
       <div className="bottomFooter-content">
         <b>Twirl © Copyright 2025</b>
       </div>
+      <div className="bottomFooter-tss"><Image src={tss} alt="tss" /></div>
       <div className="bottomFooter-content">
         <FooterSocial />
       </div>
