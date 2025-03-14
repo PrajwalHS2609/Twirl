a1bd6d1a61c59e793adb2f71b3dfd74204ce565c
Prajwal H S <prajwalhs4india@gmail.com>
2025-03-14 16:56:18 +0530

    changes

--- src/components/Whatsapp/Whatsapp.tsx
+++ src/components/Whatsapp/Whatsapp.tsx
@@ -1,21 +1,23 @@
-"use client"
-import React from 'react'
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
-import twirlLogo from "@/media/Images/logo.jpg"
-import "./Whatsapp.css"
+"use client";
+import React from "react";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import twirlLogo from "@/media/Images/logo.jpg";
+import "./Whatsapp.css";
 const Whatsapp = () => {
   return (
     <div className="whatsAppContainer">
       <FloatingWhatsApp
-        phoneNumber="919742232700"
+        phoneNumber="919972272700"
         accountName="TWIRL"
-        avatar={twirlLogo.src} // Optional
-        statusMessage="Typically replies within 5 min" // Optional
-        chatMessage="" // Optional
-        placeholder="Type a message..." // Optional
+        avatar={twirlLogo.src}
+        statusMessage="Typically replies within 5 min"
+        chatMessage="✨ Hello from Twirl Salon! ✨
+Ready to refresh your look? 💇‍♀💅 From stunning hair makeovers to luxurious manicures, pedicures, and skincare, we’ve got you covered! Book your appointment today and let us pamper you to perfection. 💖
+📍 Call or WhatsApp us at +91 9972272700 to book now!" 
+        placeholder="Type a message..."
       />
     </div>
-  )
-}
+  );
+};
 
-export default Whatsapp
+export default Whatsapp;
