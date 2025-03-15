12fed6dbdab065283f4f12fa90e0883206a6f307
Prajwal H S <prajwalhs4india@gmail.com>
2025-03-15 18:31:44 +0530

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <meta
+          name="google-site-verification"
+          content="Ny1sryXLA5O4tZ-M4lTfw8VgYAndPN2fYbXwLDAoehM"
+        />
         <link rel="icon" type="image/png" href="./favicon.png" />
       </head>
       <body>
