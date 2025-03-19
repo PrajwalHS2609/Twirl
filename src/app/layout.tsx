6240789892df31acdb4db3459a0b8ef580ca0d54
Prajwal H S <prajwalhs4india@gmail.com>
2025-03-19 16:47:16 +0530

    removed gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,23 +18,6 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <meta
-          name="google-site-verification"
-          content="Ny1sryXLA5O4tZ-M4lTfw8VgYAndPN2fYbXwLDAoehM"
-        />
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-CMCQW65YQW"
-        ></script>
-        <script>
-          {`
-           window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-CMCQW65YQW');
-  `}
-        </script>
         <link rel="icon" type="image/png" href="./favicon.png" />
       </head>
       <body>
