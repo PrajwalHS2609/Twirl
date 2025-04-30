ec552debf18a4b6d689c697fcdfa55033fafee86
Prajwal H S <prajwalhs4india@gmail.com>
2025-04-30 13:40:17 +0530

    sitemap

--- src/app/candle-spa-manicure/page.tsx
+++
@@ -1,9 +0,0 @@
-import CandleSpaManicureHead from "@/components/NailServices/CandleSpaManicure/CandleSpaManicureHead";
-
-export default function CandleSpaManicure() {
-  return (
-    <div className="main-container">
-      <CandleSpaManicureHead />
-    </div>
-  );
-}
--- src/app/eyebrow-microblading/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function MicroBlading(){
-    return(
-        <div></div>
-    )
-}
