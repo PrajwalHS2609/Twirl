9dc8d24ba7b642e8ee47fd462b1233fe1a695647
Prajwal H S <prajwalhs4india@gmail.com>
2026-03-01 17:07:44 +0530

    temporary down

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const isDown = process.env.NEXT_PUBLIC_SITE_DOWN === "true";
+  const isDown = process.env.SITE_DOWN === "true";
 
   if (isDown && request.nextUrl.pathname !== "/maintenance") {
     return NextResponse.redirect(new URL("/maintenance", request.url));
